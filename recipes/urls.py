8c32566dc46b40a7cab7d7cd68eece69a60ef277
Vinicius Oliveira <vinioliveira.web@gmail.com>
2022-12-27 11:38:32 -0300

    refactor(templates): :hammer: reorganize templates
    create the base template 'global/base.html' and extends him to render the templates 'recipes/pages/home.html' and 'recipes/pages/recipe-view.html'

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from recipes.views import home
+from . import views
 
 urlpatterns = [
-    path('', home),  # HOME
+    path('', views.home),  # HOME
+    path('recipes/<int:id>/', views.recipe),  # HOME
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):  # HOME
     return render(request, 'recipes/pages/home.html')
+
+def recipe(request, id):  # HOME
+    return render(request, 'recipes/pages/recipe-view.html')
