07a38bf2095206b852e2424386c441d28385826b
Vinicius Oliveira <vinioliveira.web@gmail.com>
2023-01-09 22:14:39 -0300

    feat(utils): :sparkles: generate placeholder with Faker
    create a function that generates a dictionary with Faker to use it as placeholder in '/utils/recipes/factory.py' and inject it on the templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,15 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):  # HOME
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
+
 
 def recipe(request, id):  # HOME
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+    })
